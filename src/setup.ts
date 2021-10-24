54c04a487518d12dfd249a6f54abe9ba87fbc099
Freddie Poser <freddie.poser@gmail.com>
2021-10-24 18:14:28 +0100

    Fix https error

--- src/setup.ts
+++ src/setup.ts
@@ -1,3 +1,3 @@
-export const PHOTOS_BASE = "http://static.vogonjeltz.com/photo_portfolio/"
+export const PHOTOS_BASE = "https://static.vogonjeltz.com/photo_portfolio/"
 export const PHOTOS_INDEX = "index.json"
 export const PHOTOS_EXT = ".jpg"
