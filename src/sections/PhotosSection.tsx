dcc463fb5bd9b8a676b8f6c61a42d321906eb0e5
Freddie Poser <freddie.poser@gmail.com>
2021-10-31 16:04:52 +0000

    Add photography logo watermark-black.png

--- src/sections/PhotosSection.tsx
+++ src/sections/PhotosSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {PhotosCard} from "../components/PhotosCard";
+import logo from "../img/watermark-black.png"
 
 export interface PhotoDefinition {
     name: string
@@ -17,6 +18,9 @@ function PhotosSection({photos}: PhotosProps) {
 
     return (
         <div className="section-content">
+            <div className="photos-header-logo-container">
+                <img src={logo} className="photos-header-logo" alt="Freddie Poser Photography logo"/>
+            </div>
             <h1>Photography Portfolio</h1>
             <div className="photos-section">
                 {photos.map((v, i, a) => <PhotosCard definition={v} index={i}/>)}
